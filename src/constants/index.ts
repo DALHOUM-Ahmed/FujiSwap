464fd5cbb483c2facb632323cd93be53b720eab5
DALHOUM-Ahmed <dalhoum.co@gmail.com>
2022-03-13 11:33:08 +0100

    default slippage set to 20%.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -173,7 +173,7 @@ export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
 
 export const NetworkContextName = 'NETWORK';
 // default allowed slippage, in bips
-export const INITIAL_ALLOWED_SLIPPAGE = 1400;
+export const INITIAL_ALLOWED_SLIPPAGE = 2000;
 // 20 minutes, denominated in seconds
 export const DEFAULT_DEADLINE_FROM_NOW = 60 * 20;
 // used for rewards deadlines
